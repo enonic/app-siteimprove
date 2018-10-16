febf20628870982d3b56a35a421f6a59cf71ed87
Thomas Sigdestad <tsi@enonic.com>
2018-10-16 10:29:06 +0200

    widget skeleton added

---
+++ src/main/resources/admin/widgets/siteimprove/siteimprove.js
@@ -0,0 +1,32 @@
+var contentLib = require('/lib/xp/content');
+var portalLib = require('/lib/xp/portal');
+var thymeleaf = require('/lib/xp/thymeleaf');
+
+function handleGet(req) {
+    var uid = req.params.uid;
+
+    var contentId = req.params.contentId;
+    if (!contentId) {
+        contentId = portalLib.getContent()._id;
+    }
+
+    var content = contentLib.get({key: contentId});
+    var site = contentLib.getSite({key: contentId});
+    var siteConfig = contentLib.getSiteConfig({key: contentId, applicationKey: app.name});
+    var pageId = "";
+
+    if (content.type.indexOf(":site") == -1 && !!site) {
+        pageId = content._path.replace(site._path, "");
+    }
+
+    var view = resolve('siteimprove.html');
+    var params = {
+        pageId: siteConfig ? pageId : -1
+    };
+
+    return {
+        contentType: 'text/html',
+        body: thymeleaf.render(view, params)
+    };
+}
+exports.get = handleGet;
