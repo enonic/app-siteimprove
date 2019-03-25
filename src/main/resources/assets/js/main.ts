7fb26396930460ef833214cd3078771c41d3e35c
Alan Semenov <ase@enonic.com>
2019-03-25 17:22:06 +0100

    Fixed config

--- src/main/resources/assets/js/main.ts
+++ src/main/resources/assets/js/main.ts
@@ -2,19 +2,17 @@ import {SiteimproveWidget} from './widget/SiteimproveWidget';
 import Path = api.rest.Path;
 
 type ConfigType = {
-    currentWidget: {
-        id: string;
-        errorMessage: string;
-        vhost: string;
-        contentPath: string;
-    }
+    widgetId: string;
+    errorMessage: string;
+    vhost: string;
+    contentPath: string
 };
 
 declare const CONFIG: ConfigType;
 
 window['HTMLImports'].whenReady(function() {
 
-    const widgetId = CONFIG.currentWidget.id;
+    const widgetId = CONFIG.widgetId;
     const widgetContainer = document.getElementById(`widget-${widgetId}`);
 
     if (widgetContainer) {
@@ -25,9 +23,9 @@ window['HTMLImports'].whenReady(function() {
         const containerEl = api.dom.Element.fromHtmlElement((container as HTMLElement), true);
 
         const widget = new SiteimproveWidget({
-            contentPath: Path.fromString(CONFIG.currentWidget.contentPath),
-            vhost: CONFIG.currentWidget.vhost,
-            errorMessage: CONFIG.currentWidget.errorMessage
+            contentPath: Path.fromString(CONFIG.contentPath),
+            vhost: CONFIG.vhost,
+            errorMessage: CONFIG.errorMessage
         });
         containerEl.appendChild(widget);
         containerEl.render();
